d5a5a2a71c93bce073294984c737ce5c4221bf9a
Katrina <k.voll-taylor@students.clark.edu>
2019-10-08 15:18:08 -0700

    Lab3 done

---
+++ Module2TuesdayDemo/Module2TuesdayDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Module2TuesdayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region simple for loop
+
+            Console.WriteLine("Simple for loop example:");
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            
+            #endregion
+
+        }
+    }
+}
